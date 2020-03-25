8a977b0ab52c34ab83b81234a033c234e36495a1
Jenovic <Jenovic@gmail.com>
2020-03-25 12:29:17 +0700

    Create Exercise4_Jensak_S.py
    Test EX4

---
+++ Assignments/Exercise4_Jensak_S.py
@@ -0,0 +1,10 @@
+FE = 88
+GB = 77
+ICS = 92
+CPM = 86
+
+print("--- Your Score ---")
+print("Foundation English",":",FE)
+print("General Business",":",GB)
+print("Introduction to Computer Systems",":",ICS)
+print("Computer Programming",":",CPM)
